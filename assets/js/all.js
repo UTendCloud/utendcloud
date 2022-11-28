dadc5b1a914c521871c68a2e3e80135b4a55b0bb
utend.cloud@gmail.com <utend.cloud@gmail.com>
2022-11-28 11:55:32 +0800

    Update 2022-11-28T03:55:28.096Z

---
+++ assets/js/all.js
@@ -0,0 +1,72 @@
+"use strict";
+
+AOS.init({
+  // Global settings:
+  disable: false,
+  // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded',
+  // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init',
+  // class applied after initialization
+  animatedClassName: 'aos-animate',
+  // class applied on animation
+  useClassNames: false,
+  // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false,
+  // disables automatic mutations' detections (advanced)
+  debounceDelay: 50,
+  // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99,
+  // the delay on throttle used while scrolling the page (advanced)
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120,
+  // offset (in px) from the original trigger point
+  delay: 0,
+  // values from 0 to 3000, with step 50ms
+  duration: 400,
+  // values from 0 to 3000, with step 50ms
+  easing: 'ease',
+  // default easing for AOS animations
+  once: false,
+  // whether animation should happen only once - while scrolling down
+  mirror: false,
+  // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
+
+}); //漢堡選單點擊
+
+$(document).ready(function () {
+  $('.hamburger').click(function () {
+    $(this).toggleClass('open');
+  });
+}); //宣告預設表單內容為空 （你想要的話也可以加東西）
+
+var initSubject = '',
+    initBody = ''; //按下傳送按鈕後執行
+
+function submitHandler() {
+  var to = "erickaivalue@gmail.com"; //寫死的傳送對象 就是公司的信箱 不會顯示在網頁上
+
+  var name = nameText.value; //讀取ID為 nameTextuser 物件中的值
+
+  var email = emailText.value;
+  var tel = telText.value;
+  var subject = subText.value; //把user填的資料都塞到 mail body 中
+
+  var body = "" + bodyText.value + '%0A%0A%0A'; //%0A是換行 換了三行
+
+  body += "From：" + nameText.value + '%0A';
+  body += "Email：" + emailText.value + '%0A';
+  body += "Tel：" + telText.value; //傳送的主要程式碼
+
+  mailTo.href = "mailto:" + to + "?subject=" + subject + "&body=" + body;
+  mailTo.click();
+} //在body onload
+
+
+function init() {
+  subText.value = initSubject;
+  toText.value = initTo;
+  bodyText.value = initBody;
+}
+//# sourceMappingURL=all.js.map
